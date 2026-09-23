379091b1dd859b8f2ea5caab8eadcbcefaaee165
Duy Lam <nguyenduylam894@gmail.com>
2026-09-23 17:38:45 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/bookingEVO/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
